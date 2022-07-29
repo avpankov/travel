310f1714ee1560dc1c2b83fd5fc5a3b1d0879d90
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-07-29 17:07:16 +0300

    feat: add basic page layout

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//Carousel
+let gallery = document.querySelector('.gallery');
+
+prev.addEventListener('click', () => {
+    gallery.style.marginLeft = '285px';
+    prev.classList.add('switcher_active');
+    middle.classList.remove('switcher_active');
+    next.classList.remove('switcher_active');
+});
+
+middle.addEventListener('click', () => {
+    gallery.style.marginLeft = '-575px';
+    prev.classList.remove('switcher_active');
+    middle.classList.add('switcher_active');
+    next.classList.remove('switcher_active');
+});
+
+next.addEventListener('click', () => {
+    gallery.style.marginLeft = '-1435px';
+    prev.classList.remove('switcher_active');
+    middle.classList.remove('switcher_active');
+    next.classList.add('switcher_active');
+})
