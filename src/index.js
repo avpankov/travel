935db9db7b3f4393274e2963985f521680d42adf
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-08-02 10:47:42 +0300

    feat: add authentication pop ups

--- src/index.js
+++ src/index.js
@@ -23,25 +23,56 @@ next.addEventListener('click', () => {
 })
 
 //Estimate scroll width
-let div = document.createElement('div');
-div.style.overflowY = 'scroll';
-div.style.width = '50px';
-div.style.height = '50px';
-document.body.append(div);
-let scrollWidth = div.offsetWidth - div.clientWidth;
-div.remove();
+function estimateScrollBarWidth() {
+    let div = document.createElement('div');
+    div.style.overflowY = 'scroll';
+    div.style.width = '50px';
+    div.style.height = '50px';
+    document.body.append(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+}
 
 //Log-in form
-logIn.addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = 'block';
-    document.querySelector('.log-in').style.marginTop = 152 + 'px';
+let overlay = document.createElement('div');
+overlay.style.cssText = `display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 102%;
+    height: 100%;
+    background: #000;
+    opacity: 0.5;
+    z-index: 2;
+    transition: display .3s;
+    overflow: hidden;`;
+
+logInBtn.addEventListener('click', () => {
+    document.body.append(overlay);
+    document.querySelector('.authentication').style.marginTop = 152 + 'px';
+    document.querySelector('.log-in').style.display = 'block';
+    document.querySelector('.sign-up').style.display = 'none';
+
     document.body.style.overflowY = 'hidden';
-    document.body.style.paddingRight = scrollWidth + 'px';
+    document.body.style.paddingRight = estimateScrollBarWidth() + 'px';
 })
 
-document.querySelector('.overlay').addEventListener('click', () => {
-    document.querySelector('.overlay').style.display = 'none';
-    document.querySelector('.log-in').style.marginTop = -800 + 'px';
+overlay.addEventListener('click', () => {
+    overlay.remove();
+    document.querySelector('.authentication').style.marginTop = -800 + 'px';
     document.body.style.overflowY = 'auto';
     document.body.style.paddingRight = 0;
+})
+
+//Register link
+registerLink.addEventListener('click', () => {
+    document.querySelector('.log-in').style.display = 'none';
+    document.querySelector('.sign-up').style.display = 'block';
+})
+
+//Log in link
+loginLink.addEventListener('click', () => {
+    document.querySelector('.log-in').style.display = 'block';
+    document.querySelector('.sign-up').style.display = 'none';
 })
