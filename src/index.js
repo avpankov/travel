f2a8c74126f9f66cd4c112cd1bc83629731f4a28
Панков Артем Владимирович <pankov_AV@GOIK.LOCAL>
2022-08-04 10:45:41 +0300

    feat: add functionality

--- src/index.js
+++ src/index.js
@@ -22,6 +22,13 @@ next.addEventListener('click', () => {
     next.classList.add('switcher_active');
 })
 
+//Carousel in mobile view
+if (document.getElementsByTagName('body')[0].clientWidth <= 390) {
+    prev.classList.add('switcher_active');
+    middle.classList.remove('switcher_active');
+    next.classList.remove('switcher_active');
+}
+
 let position = -10;
 
 arrowNext.addEventListener('click', () => {
@@ -31,13 +38,21 @@ arrowNext.addEventListener('click', () => {
     if (position == -780) {
         arrowNext.childNodes[1].setAttribute('fill-opacity', 0.5);
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 1);
-        
+        prev.classList.remove('switcher_active');
+        middle.classList.remove('switcher_active');
+        next.classList.add('switcher_active');
     } else if (position == -395) {
         arrowNext.childNodes[1].setAttribute('fill-opacity', 1);
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 1);
+        prev.classList.remove('switcher_active');
+        middle.classList.add('switcher_active');
+        next.classList.remove('switcher_active');
     } else {
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 0.5);
         arrowNext.childNodes[1].setAttribute('fill-opacity', 1);
+        prev.classList.add('switcher_active');
+        middle.classList.remove('switcher_active');
+        next.classList.remove('switcher_active');
     }
 })
 
@@ -48,32 +63,23 @@ arrowPrev.addEventListener('click', () => {
     if (position == -10) {
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 0.5);
         arrowNext.childNodes[1].setAttribute('fill-opacity', 1);
+        prev.classList.add('switcher_active');
+        middle.classList.remove('switcher_active');
+        next.classList.remove('switcher_active');
     } else if (position == -395) {
         arrowNext.childNodes[1].setAttribute('fill-opacity', 1);
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 1);
+        prev.classList.remove('switcher_active');
+        middle.classList.add('switcher_active');
+        next.classList.remove('switcher_active');
     } else {
         arrowPrev.childNodes[1].setAttribute('fill-opacity', 1);
         arrowNext.childNodes[1].setAttribute('fill-opacity', 0.5);
+        prev.classList.remove('switcher_active');
+        middle.classList.remove('switcher_active');
+        next.classList.add('switcher_active');
     }
 })
-
-arrowNext.addEventListener('click', () => {
-    switch (gallery.style.marginLeft) {
-        case '-10px':
-            prev.classList.add('switcher_active');
-            middle.classList.remove('switcher_active');
-            next.classList.remove('switcher_active');
-        case '-395px':
-            prev.classList.remove('switcher_active');
-            middle.classList.add('switcher_active');
-            next.classList.remove('switcher_active');
-        case '-780px':
-            prev.classList.remove('switcher_active');
-            middle.classList.remove('switcher_active');
-            next.classList.add('switcher_active');
-    }
-})
-
 
 //Estimate scroll width
 function estimateScrollBarWidth() {
@@ -134,12 +140,11 @@ loginLink.addEventListener('click', () => {
     document.querySelector('.sign-up').style.display = 'none';
 })
 
-//Story cut
+//Story description cut
 let descriptions = document.querySelectorAll('.story__description');
 
 if (document.getElementsByTagName('body')[0].clientWidth <= 390) {
     for (let p of descriptions) {
-        console.log(p.innerText.length);
         p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla.Lorem ipsum dolor sit a... <b style="text-decoration: underline">Read More</b>';
     }
 }
@@ -149,14 +154,14 @@ let menu = document.createElement('div');
 menu.classList.add('hamburger__menu');
 let ul = document.createElement('ul');
 ul.classList.add('hamburger__menu__list');
-ul.innerHTML = '<li>How It Works</li><li>Plan Your Trip</li><li>Destinations</li><li>Travel Stories</li><li>Account</li><li>Social Media</li>';
+ul.innerHTML = '<li>How It Works</li><li>Plan Your Trip</li><li>Destinations</li><li>Travel Stories</li><li id="account">Account</li><li>Social Media</li>';
 menu.append(ul);
-let x = document.createElement('img');
-x.src = './src/images/mobile/icons/x.png';
-x.style.cssText = `position: absolute;
+let close = document.createElement('img');
+close.src = './src/images/mobile/icons/x.png';
+close.style.cssText = `position: absolute;
     top: 15.5px;
     left: 15.5px;`;
-menu.append(x);
+menu.append(close);
 
 hamburger.addEventListener('click', () => {
     document.body.append(overlay);
@@ -166,12 +171,29 @@ hamburger.addEventListener('click', () => {
     }, 0);
 
     document.body.style.overflowY = 'clip';
+    account.addEventListener('click', () => {
+        document.body.append(overlay);
+        document.querySelector('.authentication').style.transform = 'scale(0.55)';
+        document.querySelector('.authentication').style.marginTop = 152 + 'px';
+        document.querySelector('.authentication').style.marginLeft = 15 + 'px';
+        document.querySelector('.log-in').style.display = 'block';
+        document.querySelector('.sign-up').style.display = 'none';
+    })
 })
 
-x.addEventListener('click', () => {
+close.addEventListener('click', () => {
     overlay.remove();
     menu.style.right = '-200px';
     setTimeout(() => {
         menu.remove();
     }, 900);
+})
+
+//Submit forms
+loginForm.addEventListener('submit', () => {
+    alert(`Login: ${loginEmail.value}\nPassword: ${loginPassword.value}`)
+})
+
+signupForm.addEventListener('submit', () => {
+    alert(`Login: ${signupEmail.value}\nPassword: ${signupPassword.value}`)
 })
